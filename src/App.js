1795b6e9ebf3f8f74a7760d4141422b6f5d2302a
Arian Hosseini <arianhosseini@outlook.com>
2022-10-12 21:00:49 +0330

    feat: added Bootstrap@5.2.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import UseRefExample from "./hooks/UseRefExample";
+
 import "./App.css";
 
 const App = () => {
 	return (
 		<div className="App">
-			<h1>Hello, World!</h1>
+			<h1 style={{ textAlign: "center", fontWeight: 200 }}>*** Advanced React Hooks! ***</h1>
+			<hr />
+
+			<UseRefExample />
 		</div>
 	);
 };
