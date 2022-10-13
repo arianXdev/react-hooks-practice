619697e17847d29d40a6ded5dc482bc3dbd99404
Arian Hosseini <arianhosseini@outlook.com>
2022-10-13 18:31:47 +0330

    added useRef

--- src/hooks/UseRefExample.jsx
+++ src/hooks/UseRefExample.jsx
@@ -1,10 +1,19 @@
+import { useRef, useEffect } from "react";
+
 const UseRefExample = () => {
+	const inputRef = useRef(null); // it's equal to the input element!
+
+	useEffect(() => {
+		// Why am I putting this in the useEffect? Because I want to have it after rendering
+		console.log(inputRef.current);
+	}, []); // only for mounting lifecycle | dependency array
+
 	return (
 		<div className="container">
 			<div className="d-flex flex-column justify-content-center align-items-center">
 				<h2 className="alert alert-info px-4">useRef Hook in React</h2>
 
-				<input type="text" className="form-control w-50" placeHolder="Give me a name..." />
+				<input ref={inputRef} type="text" className="form-control w-50" placeholder="Give me a name..." />
 
 				<button type="button" className="btn btn-outline-primary mt-3">
 					Focus on it
