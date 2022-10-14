f97c1e6d5db68da67af7431a8e521835ad995a9c
Arian Hosseini <arianhosseini@outlook.com>
2022-10-14 17:59:16 +0330

    improved UseMemoExample

--- src/hooks/UseMemoExample.jsx
+++ src/hooks/UseMemoExample.jsx
@@ -1,14 +1,30 @@
-import { useState, useEffect, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 
 const UseMemoExample = () => {
 	const [number, setNumber] = useState(0);
 	const [dark, setDark] = useState(false);
-	const doubleNumber = slowFunction(number);
 
-	const themeStyles = {
-		backgroundColor: dark ? "black" : "white",
-		color: dark ? "white" : "black",
-	};
+	// NOTE: Once you update the state in React, It's going to re-render your entire component!
+	// So It's going to run this entire function component from top to bottom.
+	const doubleNumber = useMemo(() => slowFunction(number), [number]);
+
+	const themeStyles = useMemo(() => {
+		return {
+			backgroundColor: dark ? "black" : "white",
+			color: dark ? "white" : "black",
+		};
+	}, [dark]);
+
+	useEffect(() => {
+		console.log("Theme Changed!");
+	}, [themeStyles]);
+
+	// const themeStyles2 = {
+	// 	backgroundColor: dark ? "black" : "white",
+	// 	color: dark ? "white" : "black",
+	// };
+
+	// console.log(themeStyles == themeStyles2); // false
 
 	return (
 		<div className="d-flex justify-content-center align-items-center flex-column">
@@ -27,7 +43,7 @@ const UseMemoExample = () => {
 
 const slowFunction = (num) => {
 	console.log("Calling Slow Function");
-	// for (let i = 0; i <= 1000000000; i++) {}
+	for (let i = 0; i <= 1000000000; i++) {}
 	return num * 2;
 };
 
