0d8b12352a6ca5da0ff2ed82b3655ad56a7e4006
Arian Hosseini <arianhosseini@outlook.com>
2022-10-17 18:44:00 +0330

    refactor UseCallbackExample Component

--- src/hooks/UseCallbackExample.jsx
+++ src/hooks/UseCallbackExample.jsx
@@ -1,48 +1,27 @@
 import { useState, useCallback, useEffect } from "react";
 
-const List = ({ getItems }) => {
-	const [items, setItems] = useState([]);
-
-	useEffect(() => {
-		setItems(getItems());
-		console.log("Updating Items ...");
-	}, [getItems]);
-
-	return items.map((item, index) => <div key={index}>{item}</div>);
-};
-
-const UseCallbackExample = () => {
-	const [number, setNumber] = useState(0);
+const UseCallbackExmaple = () => {
+	const [number, setNumber] = useState(1);
 	const [dark, setDark] = useState(false);
 
-	// NOTE: Once you update the state in React, It's going to re-render your entire component!
-	// So It's going to run this entire function component from top to bottom.
-	const getItems = useCallback(() => {
+	const getItems = () => {
 		return [number, number + 1, number + 2];
-	}, [number]);
-
-	const themeStyles = {
-		backgroundColor: dark ? "black" : "white",
-		color: dark ? "white" : "black",
 	};
 
-	useEffect(() => {
-		console.log("Theme Changed!");
-	}, [themeStyles]);
+	const theme = {
+		backgroundColor: dark ? "#333" : "#fff",
+		color: dark ? "#fff" : "#333",
+	};
 
 	return (
-		<div className="d-flex justify-content-center align-items-center flex-column">
-			<input type="number" className="form-control w-50" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} />
+		<div style={style}>
+			<input type="number" value={number} onChange={(e) => setNumber(parseInt(e.target.value))} />
 
-			<button className="btn btn-block btn-info my-3" onClick={() => setDark((prevDark) => !prevDark)}>
-				Change theme
-			</button>
+			<button onClick={() => setDark((prevDark) => !prevDark)}>Toggle Theme</button>
 
-			<div className="alert alert-warning" style={themeStyles}>
-				<List getItems={getItems} />
-			</div>
+			<List getItems={getItems} />
 		</div>
 	);
 };
 
-export default UseCallbackExample;
+export default UseCallbackExmaple;
