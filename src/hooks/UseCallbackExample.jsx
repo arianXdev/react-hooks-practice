3ded66b28f4a7382fe5a03dbd2cb2384e89186f8
Arian Hosseini <arianhosseini@outlook.com>
2022-10-21 18:44:01 +0330

    refactor: refactor useCallbackExample Component

--- src/hooks/UseCallbackExample.jsx
+++ src/hooks/UseCallbackExample.jsx
@@ -6,9 +6,9 @@ const UseCallbackExmaple = () => {
 	const [number, setNumber] = useState(1);
 	const [dark, setDark] = useState(false);
 
-	const getItems = () => {
+	const getItems = useCallback(() => {
 		return [number, number + 1, number + 2];
-	};
+	}, [number]);
 
 	const theme = {
 		backgroundColor: dark ? "#333" : "#fff",
