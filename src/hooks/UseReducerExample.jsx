fed1eb09fb4ac79dc15d40321d7e925388223803
Arian Hosseini <arianhosseini@outlook.com>
2022-10-21 19:20:54 +0330

    chore: create JSX Elements to work with useReducer

--- src/hooks/UseReducerExample.jsx
+++ src/hooks/UseReducerExample.jsx
@@ -1,3 +1,22 @@
-const UseReducerExample = () => {};
+import { useState } from "react";
+
+const UseReducerExample = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="mx-auto mt-3 d-flex align-items-center justify-content-center flex-column w-50">
+			<h5 className="alert alert-info text-center">🔱 useReducer Hook in React 🔱</h5>
+			<button className="btn btn-primary W-50" onClick={() => setCount((prevCount) => prevCount + 1)}>
+				➕ Increase
+			</button>
+			<p className="alert alert-warning text-center my-3">
+				Your counter is equal to {} <span className="badge rounded-pill bg-success"></span>
+			</p>
+			<button className="btn btn-danger W-50" onClick={() => setCount((prevCount) => prevCount - 1)}>
+				➖ Decrease
+			</button>
+		</div>
+	);
+};
 
 export default UseReducerExample;
